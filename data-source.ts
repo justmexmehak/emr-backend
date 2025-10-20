8a69a9154dc97822b2ccbb06894e9ebe2c24e1c6
justmexmehak <mehaknauman6@gmail.com>
2025-10-20 20:22:53 +0500

    env var for db

--- data-source.ts
+++ data-source.ts
@@ -3,11 +3,11 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.DB_HOST,
   port: 5432,
-  username: 'postgres',
-  password: '1234',
-  database: 'emr',
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [__dirname + '/src/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/src/migrations/*{.ts,.js}'],
   synchronize: false,
